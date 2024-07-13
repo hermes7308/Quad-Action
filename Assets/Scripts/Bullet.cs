e29c39135714a2d678f1a7d10eb1b3d22d904f7e
herme <hermes7308@gmail.com>
2024-07-13 09:59:29 -0700

    first commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage;
+    public bool isMelee;
+    public bool isRock;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (!isRock && other.gameObject.CompareTag("Floor"))
+        {
+            Destroy(gameObject, 3);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!isMelee && other.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
